5ff41befe4fdaa8f5238851bd27866802a93b363
Gaston Silvestre <gastonsilvestrebrc@gmail.com>
2024-02-05 19:07:49 -0300

    Compra de aceites terminado. Queda un par de cosas por pulir.

--- PreEntrega1_Silvestre/js/index.js
+++ PreEntrega1_Silvestre/js/index.js
@@ -1,100 +1,109 @@
-// FUNCIONES //
-
-function menu(res){
-    opcion = 0;
-
-    do{
-        opcion = parseInt(prompt("Resultado acutal: " + res +
-            "\n\nMenu de opciones\n" +
-            "\t1.Suma\n" +
-            "\t2.Resta\n" +
-            "\t3.Multiplicacion\n" +
-            "\t4.Division\n" +
-            "\t5.Salir"));
-
-        if ((opcion < 1) || (opcion > 5)){
-            alert("El numero que ingreso, no coincide con las opciones desplegadas!\n" +
-                "Por favor, vuelva a intentarlo.");
-        }
-
-    }while((opcion < 1) || (opcion > 5));
-
-    return opcion;
-}
-
-function sumar(res){
-    let numero = 0;
-    let c_num = parseInt(prompt("Ingrese la cantidad de numeros a sumar."));
-
-    for(let i=0; i < c_num; i++){
-        numero = parseInt(prompt("Numero " + (i+1) + ": "));
-        res += numero;
+function mostrarCarrito(a50L, a20L, a5L, a1500cm3, a750cm3) {
+    
+    console.log(`Carrito:\n`);
+    
+    if(a50L > 0) {
+        console.log(`\t> ${a50L} aceite/s de 50Lts\n`);
     }
 
-    return res;
-}
-
-function restar(res){
-    let numero = 0;
-    let c_num = parseInt(prompt("Ingrese la cantidad de numeros a restar."));
-
-    for(let i=0; i < c_num; i++){
-        numero = parseInt(prompt("Numero " + (i+1) + ": "));
-        if (numero > 0){
-            numero *= (-1); // lo convertimos en negativo
-        }
-        res += numero; // da igual que se sumen, ya que 'numero' es %100 negativo
+    if(a20L > 0) {
+        console.log(`\t> ${a20L} aceite/s de 20Lts`);
     }
 
-    return res;
+    if(a5L > 0) {
+        console.log(`\t> ${a5L} aceite/s de 20Lts`);
+    }
+
+    if(a1500cm3 > 0) {
+        console.log(`\t> ${a1500cm3} aceite/s de 20Lts`);
+    }
+
+    if(a750cm3 > 0) {
+        console.log(`\t> ${a750cm3} aceite/s de 20Lts`);
+    }
 }
 
-let multiplicar = res => {
-    let mult = 0; 
-    mult = parseInt(prompt("Ingrese un multiplicador."));
-    res *= mult;
-    return res;
+function continuarCompra() {
+    let continuar = '';
+    
+    do {
+        continuar = prompt('Desea continuar comprando?[s/n]');
+
+        if (continuar != 's' && continuar != 'n') {
+            console.log('Error, vuelva a intentarlo.')
+        }
+        else {
+            if (continuar == 's') {
+                console.log('Redirigiendo a la tienda...');
+            }
+            else {
+                console.log('Finalizando seleccion de productos...')
+            }
+        }
+    }while(continuar != 's' && continuar != 'n');
+
+    return continuar;
 }
 
-function dividir(res){
-    let div = 0;
-    do{
-        div = parseInt(prompt("Ingrese un divisor, distino a cero."));
-    }while(div == 0)
-    res /= div;
-    return res;
+function menu() {
+    let producto = 0;
+
+    console.log(`
+        Elegir producto:\n
+        \t1.Aceite 40L - $70.000\n
+        \t2.Aceite 20L - $40.000\n
+        \t3.Aceite 5L - $12000\n
+        \t4.Aceite 1,5L - $3000\n
+        \t5.Aceite 750cm3 - $2000\n`);
+    
+    do {
+        producto = parseInt(prompt("Elegir producto"));
+    }while(producto <= 0 || producto >= 6)
+
+    return producto;
 }
 
-// PROGRAMA PRINCIPAL //
+// Variables globales
+let seguirComprando = true;
+let decisionUsuario = '';
+let productoID = 0;
+let aceite40Lts = 0;
+let aceite20Lts = 0;
+let aceite5Lts = 0;
+let aceite1500cm3 = 0;
+let aceite750cm3 = 0;
 
-// inicializacion variables globales
+// Main
+while (seguirComprando == true) {
+    productoID = menu();
 
-let apagar = false;
-let opcionUsuario = 0;
-let resultado = 0;
-
-alert("Bienviendo a la calculadora virtual!");
-
-while(apagar == false){
-
-    opcionUsuario = menu(resultado);
-
-    switch(opcionUsuario){
+    switch (productoID) {
         case 1:
-            resultado = sumar(resultado);
+            aceite40Lts++;
             break;
         case 2:
-            resultado = restar(resultado);
+            aceite20Lts++;
             break;
         case 3:
-            resultado = multiplicar(resultado);
+            aceite5Lts++;
             break;
         case 4:
-            resultado = dividir(resultado);
+            aceite1500cm3++;
             break;
         case 5:
-            apagar = true; // salimos del programa
-            alert("Hasta la proxima!");
+            aceite750cm3++;
             break;
+    }
+
+    mostrarCarrito(aceite40Lts, aceite20Lts, aceite5Lts, aceite1500cm3, aceite5Lts);
+
+    decisionUsuario = continuarCompra();
+
+    if (decisionUsuario == 's') {
+        seguirComprando = true;
+    }
+    else {
+        seguirComprando = false;
+        console.log("Fin Programa.")
     }
 }
