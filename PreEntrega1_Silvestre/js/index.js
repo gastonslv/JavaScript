23872e2d00c9bee38a70c1812acd814e332e3c48
Gaston Silvestre <gastonsilvestrebrc@gmail.com>
2024-02-06 15:41:27 -0300

    ya corregi todos los detalles que quedaban

--- PreEntrega1_Silvestre/js/index.js
+++ PreEntrega1_Silvestre/js/index.js
@@ -1,26 +1,39 @@
-function mostrarCarrito(a50L, a20L, a5L, a1500cm3, a750cm3) {
-    
+function mostrarCarrito(a40L, a20L, a5L, a1500ml, a750ml) {
+    let total = 0;
+
     console.log(`Carrito:\n`);
     
-    if(a50L > 0) {
-        console.log(`\t> ${a50L} aceite/s de 50Lts\n`);
+    if(a40L > 0) {
+        console.log(`\t> ${a40L} aceite/s de 40Lts    -> $${a40L * 70000}\n`);
+
+        total += (a40L * 70000);
     }
 
     if(a20L > 0) {
-        console.log(`\t> ${a20L} aceite/s de 20Lts`);
+        console.log(`\t> ${a20L} aceite/s de 20Lts    -> $${a20L * 40000}`);
+
+        total += (a20L * 40000);
     }
 
     if(a5L > 0) {
-        console.log(`\t> ${a5L} aceite/s de 20Lts`);
+        console.log(`\t> ${a5L} aceite/s de 5Lts    -> $${a5L * 12000}`);
+
+        total += (a5L * 12000);
+    }
+    
+    if(a1500ml > 0) {
+        console.log(`\t> ${a1500ml} aceite/s de 1500ml    -> $${a1500ml * 3000}`);
+        
+        total += (a1500ml * 3000);
+    }
+    
+    if(a750ml > 0) {
+        console.log(`\t> ${a750ml} aceite/s de 500ml    -> $${a750ml * 2000}`);
+        
+        total += (a750ml * 2000);
     }
 
-    if(a1500cm3 > 0) {
-        console.log(`\t> ${a1500cm3} aceite/s de 20Lts`);
-    }
-
-    if(a750cm3 > 0) {
-        console.log(`\t> ${a750cm3} aceite/s de 20Lts`);
-    }
+    console.log(`\nTotal: $ ${total}`);
 }
 
 function continuarCompra() {
@@ -40,6 +53,7 @@ function continuarCompra() {
                 console.log('Finalizando seleccion de productos...')
             }
         }
+
     }while(continuar != 's' && continuar != 'n');
 
     return continuar;
@@ -47,16 +61,15 @@ function continuarCompra() {
 
 function menu() {
     let producto = 0;
-
-    console.log(`
-        Elegir producto:\n
-        \t1.Aceite 40L - $70.000\n
-        \t2.Aceite 20L - $40.000\n
-        \t3.Aceite 5L - $12000\n
-        \t4.Aceite 1,5L - $3000\n
-        \t5.Aceite 750cm3 - $2000\n`);
     
     do {
+        console.log(`
+            Elegir producto:\n
+            \t1.Aceite 40L - $70.000\n
+            \t2.Aceite 20L - $40.000\n
+            \t3.Aceite 5L - $12000\n
+            \t4.Aceite 1500ml - $3000\n
+            \t5.Aceite 750ml - $2000\n`);
         producto = parseInt(prompt("Elegir producto"));
     }while(producto <= 0 || producto >= 6)
 
@@ -70,8 +83,8 @@ let productoID = 0;
 let aceite40Lts = 0;
 let aceite20Lts = 0;
 let aceite5Lts = 0;
-let aceite1500cm3 = 0;
-let aceite750cm3 = 0;
+let aceite1500ml = 0;
+let aceite750ml = 0;
 
 // Main
 while (seguirComprando == true) {
@@ -86,16 +99,17 @@ while (seguirComprando == true) {
             break;
         case 3:
             aceite5Lts++;
+            console.log(aceite5Lts);
             break;
         case 4:
-            aceite1500cm3++;
+            aceite1500ml++;
             break;
         case 5:
-            aceite750cm3++;
+            aceite750ml++;
             break;
     }
 
-    mostrarCarrito(aceite40Lts, aceite20Lts, aceite5Lts, aceite1500cm3, aceite5Lts);
+    mostrarCarrito(aceite40Lts, aceite20Lts, aceite5Lts, aceite1500ml, aceite750ml);
 
     decisionUsuario = continuarCompra();
 
